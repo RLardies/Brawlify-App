1909164fe66d4c47b1eea4f28a9b946a235e2a07
jesusba999 <jesusba999@gmail.com>
2019-05-04 22:02:43 +0200

    Registrarse

--- src/Interfaces/Login/Registro.java
+++ src/Interfaces/Login/Registro.java
@@ -1,35 +1,89 @@
 package Interfaces.Login;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
 public class Registro extends JPanel {
-    private JLabel userLabel, passwordLabel, nameLabel;
-    private JTextField userText, nameText;
+
+
+    private JLabel nombreLabel, userLabel, passwordLabel,fechaLabel;
+    private JTextField nombreText, userText,fechaText;
     private JPasswordField passwordText;
-    private JButton registerButton;
-    private JList dia, mes, anio;
+    private JButton registroButton;
 
     public Registro() {
+
         SpringLayout layout = new SpringLayout();
         this.setLayout(layout);
 
+
+        nombreLabel = new JLabel("Nombre:");
         userLabel = new JLabel("Usuario:");
         passwordLabel = new JLabel("Contraseña:");
-        nameLabel = new JLabel("Nombre:");
+        fechaLabel = new JLabel("Fecha de Nacimiento (yyyy-mm-dd):");
+        nombreText = new JTextField(15);
         userText = new JTextField(15);
-        nameText = new JTextField(15);
         passwordText = new JPasswordField(15);
-        registerButton = new JButton("Registrarse");
+        fechaText = new JTextField(15);
+        registroButton = new JButton("Registrarse");
 
 
+        layout.putConstraint(SpringLayout.WEST, nombreLabel, 120, SpringLayout.WEST, this);
+        layout.putConstraint(SpringLayout.NORTH, nombreLabel, 155, SpringLayout.NORTH, this);
 
-        dia.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        mes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        anio.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        layout.putConstraint(SpringLayout.WEST, nombreText, 190, SpringLayout.EAST, nombreLabel);
+        layout.putConstraint(SpringLayout.NORTH, nombreText, -2, SpringLayout.NORTH, nombreLabel);
 
-        JScrollPane scrollDia = new JScrollPane(dia);
-        JScrollPane scrollMes = new JScrollPane(mes);
-        JScrollPane scrollAnio = new JScrollPane(anio);
+        layout.putConstraint(SpringLayout.WEST, userLabel, 0, SpringLayout.WEST, nombreLabel);
+        layout.putConstraint(SpringLayout.NORTH, userLabel, 30, SpringLayout.SOUTH, nombreLabel);
 
+        layout.putConstraint(SpringLayout.WEST, userText, 0, SpringLayout.WEST, nombreText);
+        layout.putConstraint(SpringLayout.NORTH, userText, -2, SpringLayout.NORTH, userLabel);
+
+        layout.putConstraint(SpringLayout.WEST, passwordLabel, 0, SpringLayout.WEST, userLabel);
+        layout.putConstraint(SpringLayout.NORTH, passwordLabel, 30, SpringLayout.SOUTH, userLabel);
+
+        layout.putConstraint(SpringLayout.WEST, passwordText, 0, SpringLayout.WEST, userText);
+        layout.putConstraint(SpringLayout.NORTH, passwordText, -2, SpringLayout.NORTH, passwordLabel);
+
+        layout.putConstraint(SpringLayout.WEST, fechaLabel, 0, SpringLayout.WEST, userLabel);
+        layout.putConstraint(SpringLayout.NORTH, fechaLabel, 30, SpringLayout.SOUTH, passwordLabel);
+
+        layout.putConstraint(SpringLayout.WEST, fechaText, 0, SpringLayout.WEST, userText);
+        layout.putConstraint(SpringLayout.NORTH, fechaText, -2, SpringLayout.NORTH, fechaLabel);
+
+        layout.putConstraint(SpringLayout.WEST, registroButton, 0, SpringLayout.WEST, userLabel);
+        layout.putConstraint(SpringLayout.NORTH, registroButton, 30, SpringLayout.SOUTH, fechaLabel);
+
+
+        this.add(nombreLabel);
+        this.add(nombreText);
+        this.add(userLabel);
+        this.add(userText);
+        this.add(passwordLabel);
+        this.add(passwordText);
+        this.add(fechaLabel);
+        this.add(fechaText);
+        this.add(registroButton);
+
+        this.setPreferredSize(new Dimension(950, 600));
     }
+
+    public void setControlador(ActionListener c) {
+        registroButton.setActionCommand("Registrarse");
+        registroButton.addActionListener(c);
+    }
+
+    public String getUserText() {
+        return userText.getText();
+    }
+
+    public String getPasswordText() {
+        return passwordText.getText();
+    }
+
+    public String getNombretext(){ return nombreText.getText(); }
+
+    public String getFechaText(){ return fechaText.getText(); }
 }
