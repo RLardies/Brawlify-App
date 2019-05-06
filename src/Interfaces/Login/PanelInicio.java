693565e9efa586892328b5c70efe580f8625e4b3
Lardis <lardiesrodrigo@gmail.com>
2019-05-06 15:09:12 +0200

    v

--- src/Interfaces/Login/PanelInicio.java
+++ src/Interfaces/Login/PanelInicio.java
@@ -19,7 +19,9 @@ import java.awt.event.ActionListener;
     }
 
     public void setControlador(ActionListener c) {
+
         login.setControlador(c);
+        registro.setControlador(c);
     }
 
 
